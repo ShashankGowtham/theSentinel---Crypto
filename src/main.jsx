0f7dd50961c5b2dfc3eb396ab85b2e500bfce0de
Shashank Gowtham <shashankgowthamjula@gmail.com>
2025-10-12 12:05:56 +0530

    changes for publishing made 1

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
+import { BrowserRouter } from "react-router-dom"; // 👈 import BrowserRouter
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <BrowserRouter basename="/cryptocurrency"> {/* 👈 set basename */}
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/cryptocurrency/', // 👈 Add this line — use your repo name here
 })
